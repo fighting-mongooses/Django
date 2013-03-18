22e58a93fedc7381896a6eb878a78e62a311119b
Tom Mason <wheybags@wheybags.com>
2013-03-18 13:00:24 +0000

    date fix

--- Androkon/androkon/conference/forms.py
+++ Androkon/androkon/conference/forms.py
@@ -7,8 +7,8 @@ class ConferenceForm(ModelForm):
 
 	name 		= forms.CharField(label=(u'Name of the Conference'))
 	description = forms.CharField(label=(u'Description of the Conference'), widget=forms.Textarea())
-	start_date	= forms.DateField(("%d/%m/%Y %H:%M:%S",), label=(u'Start Date of the Conference'), widget=forms.DateTimeInput(attrs={'style' : 'height : 30px'}, format="%d/%m/%Y %H:%M:%S"))
-	end_date	= forms.DateField(("%d/%m/%Y %H:%M:%S",), label=(u'End Date of the Conference'), widget=forms.DateTimeInput(attrs={'style' : 'height : 30px'}, format="%d/%m/%Y %H:%M:%S"))
+	start_date	= forms.DateTimeField(("%d/%m/%Y %H:%M:%S",), label=(u'Start Date of the Conference'), widget=forms.DateTimeInput(attrs={'style' : 'height : 30px'}, format="%d/%m/%Y %H:%M:%S"))
+	end_date	= forms.DateTimeField(("%d/%m/%Y %H:%M:%S",), label=(u'End Date of the Conference'), widget=forms.DateTimeInput(attrs={'style' : 'height : 30px'}, format="%d/%m/%Y %H:%M:%S"))
 	twitter		= forms.CharField(label=(u'Username of the Twitter account associated with the event'))
 	website		= forms.URLField(label=(u'Website for the Conference'))
 	guests		= forms.CharField(label=(u'Description of guest speakers or special guests attending the event'), widget=forms.Textarea(), required=False)
