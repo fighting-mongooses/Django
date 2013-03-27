04424e46237d673ecf8fa43692d905350e488ea2
Tom Mason <wheybags@wheybags.com>
2013-03-27 12:00:22 +0000

    Users can edit existing conferences

--- Androkon/androkon/conference/views.py
+++ Androkon/androkon/conference/views.py
@@ -12,12 +12,12 @@ from django.template.defaultfilters import slugify
 #	entries = Conference.objects.get(user= )
 
 def json_cons(request):
-    json = serializers.serialize("json", Conference.objects.filter(enabled = True))
-    return HttpResponse(json, mimetype='application/json')
+	json = serializers.serialize("json", Conference.objects.filter(enabled = True))
+	return HttpResponse(json, mimetype='application/json')
 
 def json_events(request):
-    json = serializers.serialize("json", Event.objects.all())
-    return HttpResponse(json, mimetype='application/json')
+	json = serializers.serialize("json", Event.objects.all())
+	return HttpResponse(json, mimetype='application/json')
 
 
 def ConferenceRegistration(request):
@@ -51,37 +51,79 @@ def ConferenceRegistration(request):
 		context = {'form': form, 'baseUrl': baseUrl}
 		return render_to_response('reg_con.html', context, context_instance=RequestContext(request))
 
+def ConferenceUpdate(request, key):
+	baseUrl = "../../"
+	
+	try:
+		con = Conference.objects.get(pk=key)
+	except Conference.DoesNotExist:
+		context = {'baseUrl': baseUrl, 'message': 'Invalid con id.'}
+		return render_to_response('denied.html', context, context_instance=RequestContext(request))
+	
+	if request.user == con.user or request.user.is_superuser:
+		if request.method =='POST':
+			form = ConferenceForm(request.POST)
+			if form.is_valid():
+				con.name = form.cleaned_data['name']
+				con.description = form.cleaned_data['description']
+				con.start_date = form.cleaned_data['start_date']
+				con.end_date = form.cleaned_data['end_date']
+				con.twitter = form.cleaned_data['twitter']
+				con.website = form.cleaned_data['website']
+				con.guests = form.cleaned_data['guests']
+				con.save()
+				return HttpResponseRedirect(baseUrl + 'profile/')
+
+			else:
+				context = { 'form' : form, 'baseUrl' : baseUrl }
+				return render_to_response('reg_con.html', context, context_instance=RequestContext(request))
+
+		else:
+			form = ConferenceForm({
+				'name': con.name, 'description': con.description, 'start_date': con.start_date,
+				'end_date': con.end_date, 'twitter': con.twitter, 'website': con.website, 'guests': con.guests})
+
+			context = { 'form' : form, 'baseUrl' : baseUrl }
+			return render_to_response('reg_con.html', context, context_instance=RequestContext(request))
+	 
+ 
+	else:
+		context = {'baseUrl': baseUrl, 'message': 'You do not have permission to edit this conference.'}
+		return render_to_response('denied.html', context, context_instance=RequestContext(request))
+
+   
 def DeleteCon(request, key):
-    baseUrl = "../../"
-    
-    try:
-        con = Conference.objects.get(pk=key)
-    except Conference.DoesNotExist:
-        context = {'baseUrl': baseUrl, 'message': 'Invalid con id.'}
-        return render_to_response('denied.html', context, context_instance=RequestContext(request))
-        
-    if request.user == con.user or request.user.is_superuser:
-        con.enabled = False
-        con.save()
-        return HttpResponseRedirect(baseUrl + "profile/")
-    else:
-        context = {'baseUrl': baseUrl, 'message': 'You do not have permission to delete this conference.'}
-        return render_to_response('denied.html', context, context_instance=RequestContext(request))
+	baseUrl = "../../"
+	
+	try:
+		con = Conference.objects.get(pk=key)
+	except Conference.DoesNotExist:
+		context = {'baseUrl': baseUrl, 'message': 'Invalid con id.'}
+		return render_to_response('denied.html', context, context_instance=RequestContext(request))
+		
+	if request.user == con.user or request.user.is_superuser:
+		con.enabled = False
+		con.save()
+		return HttpResponseRedirect(baseUrl + "profile/")
+	else:
+		context = {'baseUrl': baseUrl, 'message': 'You do not have permission to delete this conference.'}
+		return render_to_response('denied.html', context, context_instance=RequestContext(request))
 
 
 def RestoreCon(request, key):
-    baseUrl = "../../"
-    
-    try:
-        con = Conference.objects.get(pk=key)
-    except Conference.DoesNotExist:
-        context = {'baseUrl': baseUrl, 'message': 'Invalid con id.'}
-        return render_to_response('denied.html', context, context_instance=RequestContext(request))
-        
-    if request.user == con.user or request.user.is_superuser:
-        con.enabled = True
-        con.save()
-        return HttpResponseRedirect(baseUrl + "profile/")
-    else:
-        context = {'baseUrl': baseUrl, 'message': 'You do not have permission to delete this conference.'}
-        return render_to_response('denied.html', context, context_instance=RequestContext(request))
+	baseUrl = "../../"
+	
+	try:
+		con = Conference.objects.get(pk=key)
+	except Conference.DoesNotExist:
+		context = {'baseUrl': baseUrl, 'message': 'Invalid con id.'}
+		return render_to_response('denied.html', context, context_instance=RequestContext(request))
+		
+	if request.user == con.user or request.user.is_superuser:
+		con.enabled = True
+		con.save()
+		return HttpResponseRedirect(baseUrl + "profile/")
+	else:
+		context = {'baseUrl': baseUrl, 'message': 'You do not have permission to delete this conference.'}
+		return render_to_response('denied.html', context, context_instance=RequestContext(request))
+
